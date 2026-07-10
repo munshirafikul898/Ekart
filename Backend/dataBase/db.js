4d56ad52a42c4e9261ab31bd530c2f022d3cbc95
munshirafikul898 <munshirafikul898@gmail.com>
2026-07-10 18:54:21 +0530

    Fix MongoDB connection

--- Backend/dataBase/db.js
+++ Backend/dataBase/db.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 const connectDB=async ()=>{
     try {
-        await mongoose.connect(`${process.env.MONGO_URL}/Ekart`);
+        await mongoose.connect(`${process.env.MONGO_URL}`);
         console.log("Database connect successfully."); 
     } catch (error) {
         console.log("DB connection fail.",error);      
