39daf0b88c6a1c9243d21a7429c3de5b95133147
munshirafikul898 <munshirafikul898@gmail.com>
2026-07-10 21:34:10 +0530

    Change SMTP port

--- Backend/mailVerify/verifyEmail.js
+++ Backend/mailVerify/verifyEmail.js
@@ -5,8 +5,8 @@ export const verifyEmail = async (email, token) => {
     try {
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS
